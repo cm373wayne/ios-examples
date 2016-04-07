cf2378c73ce037039bc62192a4cacc5c40f4e3c3
Wayne Chang <wayne@cmoney.net.tw>
2016-04-07 11:42:05 +0800

    Text Kit Tutorial

---
+++ TextKitNotepad/TextKitNotepad/SyntaxHighlightTextStorage.h
@@ -0,0 +1,13 @@
+//
+//  SyntaxHighlightTextStorage.h
+//  TextKitNotepad
+//
+//  Created by macmini03 on 2016/4/7.
+//  Copyright © 2016年 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyntaxHighlightTextStorage : NSTextStorage
+
+@end
